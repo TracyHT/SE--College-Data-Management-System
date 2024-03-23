6938037a96e09dd462c33d781d22746ef2fc98f7
kyshihixd <120297075+kyshihixd@users.noreply.github.com>
2024-03-23 23:28:31 +0700

    Add files via upload

---
+++ src/mainImage.jsx
@@ -0,0 +1,11 @@
+import part from "./assets/loginPage.png"
+
+function mainImage(){
+    return  (
+        <div>
+            <img className="mainPic" src={part}></img>
+        </div>
+    )
+}
+
+export default mainImage
