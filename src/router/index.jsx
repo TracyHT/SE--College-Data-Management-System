7b395c66a16ce2b48d17c2af418beed1574f5a71
Dinh Thi Thanh Nha <89521781+DiNha31@users.noreply.github.com>
2024-04-28 20:01:33 +0700

    Add files via upload

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,22 @@
 import { Route,createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Login from "../pages/auth/login";
+import Overview from "../pages/site/Overview";
+import DetailsNotification from "../pages/notification/DetailsNotification";
+import Notification from "../pages/notification";
+import NoticeBoard from "../pages/notification/NoticeBoard";
+import NoticeBoardDetails from "../pages/notification/notice-board-detail";
+import Gradebook from "../pages/Gradebook";
 
 const router =createBrowserRouter(
     createRoutesFromElements(
       <Route>
-          <Route path="/login" element={<Login />}  />
+          <Route path="/" element={<Overview/>}  />
+          <Route path="login" element={<Login />}  />
+          <Route path="notification" element={<Notification/>} />
+          <Route path="notification/:slug"  element={<DetailsNotification/>} />
+          <Route path="notification/notice-board"  element={<NoticeBoard/>} />
+          <Route path="notification/notice-board/:slug"  element={<NoticeBoardDetails/>} />
+          <Route path="gradebook"  element={<Gradebook/>} />
       </Route>
     )
   )
