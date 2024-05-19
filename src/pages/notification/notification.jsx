34d82b40f1496848fa4635199ccbf75e989c48c3
kyshihixd <120297075+kyshihixd@users.noreply.github.com>
2024-05-19 19:37:08 +0700

    added timetable page

--- src/pages/notification/notification.jsx
+++ src/pages/notification/notification.jsx
@@ -1,10 +1,11 @@
 import LayoutLogged from '../../layouts/layout-logged';
 import PropTypes from 'prop-types';
-
+import RouterNoti from './routerNoti';
 
 export default function Notification({children}) {
 
     return <LayoutLogged label={"Notification"} >
+        <RouterNoti />
         {children}
     </LayoutLogged>
 }
