885216f70d92d2d394c874d6dae66fe19c184de8
Dinh Thi Thanh Nha <89521781+DiNha31@users.noreply.github.com>
2024-05-20 23:32:28 +0700

    change router

--- layouts/layout-empty.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-export default function LayoutEmpty ({ children  }){
-    return (<div className="md:container mx-auto">
-        {children}
-    </div>)
-}
-LayoutEmpty.propTypes = {
-    children: PropTypes.node.isRequired // Ensure children prop is provided and of type node
-};
